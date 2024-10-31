0578b122b1a5701184fd5d0005ddf18a0cc0c691
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2024-10-31 00:05:46 -0400

    added curl

--- server/main.c
+++ server/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <sqlite3.h>
+#include <curl/curl.h>
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
   int i;
   for(i=0; i<argc; i++){
-    printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    printf("%s\n", argv[i] ? argv[i] : "NULL");
   }
   printf("\n");
   return 0;
@@ -13,23 +14,27 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
 int main(int argc, char **argv){
   sqlite3 *db;
   char *zErrMsg = 0;
-  int rc;
-
-  if( argc!=3 ){
-    fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
-    return(1);
-  }
-  rc = sqlite3_open(argv[1], &db);
-  if( rc ){
-    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-    sqlite3_close(db);
-    return(1);
-  }
-  rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
-  if( rc!=SQLITE_OK ){
-    fprintf(stderr, "SQL error: %s\n", zErrMsg);
-    sqlite3_free(zErrMsg);
-  }
+  sqlite3_open("server.db", &db);
+  sqlite3_exec(db, "select * from test", callback, 0, &zErrMsg);
   sqlite3_close(db);
+
+  CURL *curl = curl_easy_init();
+  if(curl) {
+    CURLcode res;
+    curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.42.0");
+    curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
+    curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
+    curl_easy_setopt(curl, CURLOPT_URL, "https://api.weather.gov/gridpoints/PHI/59,90/forecast");
+
+    res = curl_easy_perform(curl);
+    if(res != CURLE_OK) {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    }
+
+    curl_easy_cleanup(curl);  // Clean up the session
+  }
+
+  curl_global_cleanup();
   return 0;
 }
